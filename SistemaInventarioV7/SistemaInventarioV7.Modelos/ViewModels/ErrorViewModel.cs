af056a493fae56932e7ce755bee71a3850ba0b4f
ciso2020 <98385886+ciso2020@users.noreply.github.com>
2025-02-13 22:03:10 -0600

    Delete SistemaInventarioV7 directory

--- SistemaInventarioV7/SistemaInventarioV7.Modelos/ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SistemaInventarioV7.Modelos.ViewModels
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
